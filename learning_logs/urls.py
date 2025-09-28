def48aaabc43e2be1795c8f81276e48e166c72fc
Carlos Briceno <carlosbriceno33.2@gmail.com>
2025-09-27 17:55:19 -0700

    Added basic pages

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -5,7 +5,10 @@ from django.urls import path  # Imported the path function
 from . import views  # Imported the views module 
 
 app_name = 'learning_logs'
-urlspatterns = [
+urlpatterns = [      # List of pages that can requested from the learning_logs app 
     # Home page
-    path('', views.index, name='index')
+    path('', views.index, name='index'),  # Gives 3 arguments 
+    path('topics/', views.topics, name='topics'),
+    path('topics/<int:topic_id>/', views.topic, name='topic'),
+    
 ]
